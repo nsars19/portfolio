a3361076170eb9ce8d9d3776d10266091cc3d2ee
Nick S <nsardonini@gmail.com>
2021-04-26 16:26:59 -0400

    add global styles to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import colors from "./colors";
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${colors.background}
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
