d6d9bc44d0990b64b623a495b18910e062206384
Nick S <nsardonini@gmail.com>
2021-05-04 14:04:32 -0400

    add backgroun color & text color

--- src/colors.js
+++ src/colors.js
@@ -1,8 +1,9 @@
 const colors = {
-  background: "#1b262c",
+  background: "#1b1a17",
   main: "#0f4c75",
   accent: "#3282b8",
   accentLight: "#bbe1fa",
+  text: "#f0f0f0",
 };
 
 export default colors;
