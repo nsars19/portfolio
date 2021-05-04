c0424076e220262c8b06246a14a8f4167d809ce2
Nick S <nsardonini@gmail.com>
2021-05-04 16:37:11 -0400

    add scroll target names to app section components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import Intro from "./components/intro";
+import Intro from "./components/intro/intro";
+import Projects from "./components/projects/projects";
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+import { Element } from "react-scroll";
 
 function App() {
   return (
     <>
       <Intro />
+      <Element name="projects">
+        <Projects />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
     </>
   );
 }
