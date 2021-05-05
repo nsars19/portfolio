d90b50c4904da07f25b85778d78569171e49ae3e
Nick S <nsardonini@gmail.com>
2021-05-05 13:21:54 -0400

    add project detail file
    exports an array that holds objects representing project details

---
+++ src/components/projects/projDetail.js
@@ -0,0 +1,56 @@
+import facespace from "./assets/facespace1.png";
+import atphotos from "./assets/atphotos.png";
+import knight from "./assets/knight.png";
+import bst from "./assets/bst.png";
+import chess from "./assets/chess.png";
+import waldo from "./assets/waldo.png";
+
+export const detail = [
+  {
+    title: "Facespace",
+    src: facespace,
+    desc:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    tags: ["react", "express", "node", "heroku", "mongoDB", "s3"],
+    code: "https://github.com/nsars19/facebookish",
+    live: "https://nsars19.github.io/facebookish",
+  },
+  {
+    title: "AT Photos",
+    src: atphotos,
+    desc: "Description",
+    tags: ["react", "express", "node", "heroku", "mongoDB", "s3"],
+    code: "https://github.com/nsars19/at-photos",
+    live: "https://nsars19.github.io/at-photos",
+  },
+  {
+    title: "Knight's Travails",
+    src: knight,
+    desc:
+      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    tags: ["ruby", "data structures", "algorithms"],
+    code: "https://github.com/nsars19/knight-s-travails",
+  },
+  {
+    title: "Binary Search Trees",
+    src: bst,
+    desc: "description",
+    tags: ["ruby", "data structures", "algorithms"],
+    code: "https://github.com/nsars19/Binary-Search-Trees",
+  },
+  {
+    title: "Chess",
+    src: chess,
+    desc: "description",
+    tags: ["ruby", "chess", "oop"],
+    code: "https://github.com/nsars19/Chess",
+  },
+  {
+    title: "Image Tagger",
+    src: waldo,
+    desc: "description",
+    tags: ["react", "rails", "heroku", "sql"],
+    code: "https://github.com/nsars19/Image-Tagger",
+    live: "https://nsars19.github.io/Image-Tagger/",
+  },
+];
