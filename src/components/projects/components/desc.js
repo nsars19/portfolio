255b3f7eb5245d668c51d5b6dbaf330ae86c9fca
Nick S <nsardonini@gmail.com>
2021-05-19 11:42:08 -0400

    center align description text at mobile screen sizes

--- src/components/projects/components/desc.js
+++ src/components/projects/components/desc.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const StyledDescription = styled.p`
   max-width: 500px;
+
+  @media (max-width: 1024px) {
+    text-align: center;
+  }
 `;
 
 export default function Description({ text }) {
